fb6dbaebadacaa4795234ca4bcb2530bf2c4de35
Germano Manzoni <germano12344@gmail.com>
2024-10-25 19:51:50 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,5 +30,10 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("\n=== Teste 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted. New Id = " + newSeller.getId());
+
     }
 }
